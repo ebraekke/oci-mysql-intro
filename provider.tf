5f1c4022cf89402177cd098a8904952cc804c1c8
Espen Brækken <ebraekke@yahoo.com>
2023-02-20 14:53:06 +0100

    changd to session based auth (2)

--- provider.tf
+++ provider.tf
@@ -1,16 +1,6 @@
-/*
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
-}
-*/
 
-# TODO: parameterize
 provider "oci" {
   auth = "SecurityToken"
-  config_file_profile = "nosefra"
-  region           = var.region
+  config_file_profile = var.oci_cli_profile
+  region              = var.region
 }
